9ee8424a8195f1715205eb00180b150ae1b77c23
Jensen Nguyen <jngu24000@gmail.com>
2024-07-18 18:08:42 -0400

    Fixed code for Nair

--- src/Aerials/AttackAirN/mod.rs
+++ src/Aerials/AttackAirN/mod.rs
@@ -17,19 +17,6 @@ unsafe extern "C" fn effect_attackairn_limit(agent: &mut L2CAgentBase) {
             agent.module_accessor,
             *FIGHTER_CLOUD_INSTANCE_WORK_ID_FLAG_LIMIT_BREAK,
         ) {
-            macros::EFFECT_FOLLOW_WORK(
-                agent,
-                *FIGHTER_CLOUD_INSTANCE_WORK_ID_INT_EFFECT_KIND_SWORD_FLARE,
-                Hash40::new("haver"),
-                0,
-                0,
-                0,
-                0,
-                0,
-                0,
-                1,
-                true,
-            );
             macros::LAST_EFFECT_SET_OFFSET_TO_CAMERA_FLAT(agent, 0.2);
             macros::AFTER_IMAGE4_ON_arg29(
                 agent,
